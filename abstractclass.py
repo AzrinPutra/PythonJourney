5e4a62b2882e134f53cb581f8fe70039f1368e0e
Azrin Putra <i.am.azrin@gmail.com>
2025-11-29 12:05:10 +0800

    (update)

--- abstractclass.py
+++ abstractclass.py
@@ -1,4 +1,10 @@
 #!/usr/env/bin python3
+# To test this code in terminal, go to the folder that has this py file
+# Run python3
+# from collections.abc import Container
+# In Python3, collections is deprecated and moved to ollections.abc
+# from abstractclass import OddContainer
+# This extracts the class OddContainer
 
 class OddContainer:
     def __contains__(self, x):
