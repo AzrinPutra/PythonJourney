5a4221fae6269e9ef55bc0d8f8ad880f6b3862d8
Azrin Syalihin Putra Bin Surani <34625681+AzrinPutra@users.noreply.github.com>
2025-12-10 17:41:09 +0800

    (Update) TestKwargs4.py

--- TestKwargs4.py
+++ TestKwargs4.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 class Product:
-    def __init__(self, name='', price='', **kwargs):
+    def __init__(self, name='', price=None, **kwargs):
         self.name = name
         self.price = price
         self.extra_att = kwargs
@@ -13,13 +13,21 @@ class Product:
         for key, value in self.extra_att.items():
             print(f"{key}:{value}")
             
+    def apply_discount(self):
+        if "discount" in self.extra_att:
+            discount_percent = self.extra_att("discount")
+            self.price = self.price * (1 - discount_percent / 100)
+            print(f"New discounted price: {self.price}")
+        else:
+            print(f"No discount available")
+           
 class Electronics(Product):
-    def __init__(self, name='', price='', warranty='', **kwargs):
+    def __init__(self, name='', price=None , warranty='', **kwargs):
         super().__init__(name=name, price=price)
         self.warranty = warranty
         
 class Clothing(Product):
-    def __init__(self, name='', price='', size='', **kwargs):
+    def __init__(self, name='', price=None, size='', **kwargs):
         super().__init__(name=name, price=price)
         self.size = size
         
